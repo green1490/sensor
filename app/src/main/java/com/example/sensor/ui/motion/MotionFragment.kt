359dc807878061ff4fa8847bb01ce49bdb2b1fe1
green1490 <zambokrisztian1490@gmail.com>
2022-07-02 18:42:35 +0200

    clean up

--- app/src/main/java/com/example/sensor/ui/motion/MotionFragment.kt
+++ app/src/main/java/com/example/sensor/ui/motion/MotionFragment.kt
@@ -6,7 +6,6 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
