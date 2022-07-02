3b62d22e8188bba82f2d5dce1e1b2581fab9a249
green1490 <zambokrisztian1490@gmail.com>
2022-07-02 18:43:35 +0200

    clean up and crash fixing

--- app/src/main/java/com/example/sensor/MainActivity.kt
+++ app/src/main/java/com/example/sensor/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.sensor
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.sensor.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-//    TO DO: gyroscope
-//    rotation vector
-//    step counter
 
     private lateinit var binding: ActivityMainBinding
 
@@ -24,8 +21,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
